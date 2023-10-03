7dc97649acef947ec59078c1a6d1216076bc4d97
Greg <gjmooney@asu.edu>
2023-10-03 17:07:45 +0200

    Add delete files

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 import { privateProcedure, publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
@@ -38,6 +39,30 @@ export const appRouter = router({
       },
     });
   }),
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 // Export type router type signature,
 // NOT the router itself.
