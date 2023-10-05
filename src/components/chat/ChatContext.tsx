8f5327f216e7bc0571c00044111f4515aff29ace
Greg <gjmooney@asu.edu>
2023-10-05 16:16:57 +0200

    Set up chat context

---
+++ src/components/chat/ChatContext.tsx
@@ -0,0 +1,65 @@
+import { useMutation } from "@tanstack/react-query";
+import { ChangeEvent, ReactNode, createContext, useState } from "react";
+import { useToast } from "../ui/use-toast";
+
+type TChatContext = {
+  addMessage: () => void;
+  message: string;
+  handleInputChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+  isLoading: boolean;
+};
+
+const ChatContext = createContext<TChatContext>({
+  addMessage: () => {},
+  message: "",
+  handleInputChange: () => {},
+  isLoading: false,
+});
+
+interface ChatContextProps {
+  fileId: string;
+  children: ReactNode;
+}
+
+export const ChatContextProvider = ({ fileId, children }: ChatContextProps) => {
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { toast } = useToast();
+
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: async ({ message }: { message: string }) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        body: JSON.stringify({
+          fileId,
+          message,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to send message");
+      }
+
+      return response.body;
+    },
+  });
+
+  const addMessage = () => sendMessage({ message });
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  return (
+    <ChatContext.Provider
+      value={{
+        addMessage,
+        message,
+        handleInputChange,
+        isLoading,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
