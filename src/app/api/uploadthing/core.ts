aca234cb4ee0105123dd2f26a1e9a16b8cd46fe5
Greg <gjmooney@asu.edu>
2023-10-05 18:34:23 +0200

    Set up vectorization

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,9 @@
 import { db } from "@/db";
+import { pinecone } from "@/lib/pinecone";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { CohereEmbeddings } from "langchain/embeddings/cohere";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
@@ -25,6 +29,55 @@ export const ourFileRouter = {
           uploadStatus: "PROCESSING",
         },
       });
+
+      try {
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+        // get pdf blob
+        const blob = await response.blob();
+
+        // load blob into memory
+        const loader = new PDFLoader(blob);
+
+        // create document array from blob
+        const pageLevelDocs = await loader.load();
+        const pagesAmount = pageLevelDocs.length;
+
+        // add file Id to document metadata
+        pageLevelDocs.forEach((page) => {
+          page.metadata["fileId"] = createdFile.id;
+        });
+
+        // vectorize and index document
+        const pineconeIndex = pinecone.Index("feather");
+        const embeddings = new CohereEmbeddings({
+          apiKey: process.env.COHERE_API_KEY!,
+        });
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+        });
+
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (error) {
+        console.log("error", error);
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
